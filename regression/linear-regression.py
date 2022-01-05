ccadf2655f94b732caec23f85cc5f73243fd9645
cuit-sirb-wangtao <78524771+cuit-sirb-wangtao@users.noreply.github.com>
2022-01-05 22:35:15 +0800

    Update linear-regression.py

--- regression/linear-regression.py
+++ regression/linear-regression.py
@@ -34,4 +34,42 @@ batch_size = 10
 for X, y in data_iter(batch_size, features, labels):
     print(X, '\n', y)
     break;
+"""初始化模型参数"""
+w = torch.normal(0, 0.01, size=(2,1), requires_grad=True)
+b = torch.zeros(1, requires_grad=True)
+"""定义模型"""
+def linreg(X, w, b):
+    return torch.matmul(X, w) + b
+"""定义损失函数"""
+def squared_loss(y_hat, y):
+    return (y_hat - y.reshape(y_hat.shape))**2 / 2
+"""定义优化算法"""
+def sgd(params, lr, batch_size):
+    with torch.no_grad():
+        for param in params:
+            param -= lr * param.grad / batch_size
+            param.grad.zero_()
+
+"""
+训练"""
+lr = 0.03
+num_epochs = 3
+net = linreg
+loss = squared_loss
+
+for epoch in range(num_epochs):
+    for X, y in data_iter(batch_size, features, labels):
+        l = loss(net(X, w, b), y)
+        l.sum().backward()
+        sgd([w, b], lr, batch_size)
+    with torch.no_grad():
+        train_l = loss(net(features, w, b), labels)
+        print(f'epoch {epoch +1}, loss {float(train_l.mean()):f}')
+
+print(f'w的估计误差： {true_w - w.reshape(true_w.shape)}')
+print(f'b的估计误差： {true_b -b}')
+
+
+    
+
     
