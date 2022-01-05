e46da4767a3040399d291d9c74f2ec90504e0a9d
cuit-sirb-wangtao <78524771+cuit-sirb-wangtao@users.noreply.github.com>
2022-01-05 20:10:00 +0800

    Update linear-regression.py

--- regression/linear-regression.py
+++ regression/linear-regression.py
@@ -1,1 +1,17 @@
+import random
+import torch
+from d2l import torch as d2l
+
+"""定义线性函数"""
+def synthetic_data(w, b, num_examples):
+    """生成y=Xw+b+噪声"""
+    X = torch.normal(0, 1, (num_examples, len(w)))
+    y = torch.matmul(X, w) + b
+    y += torch.normal(0, 0.1, y.shape)
+    return X, y.reshape(-1, 1)
+"""实例化线性函数"""
+"""生成数据样本和标签"""
+true_w = torch.tensor([2, -3.4])
+true_b = 4.2
+features, labels = synthetic_data(true_w, true_b, 1000)
 
